2431bd6efab61888e7e4cca0b420b65977040c3b
luukkonen1991 <luukkonen1991@gmail.com>
2019-11-19 11:24:20 +0200

    Get for different questions by topic category added

--- jeopardy-server/routes/questions.js
+++ jeopardy-server/routes/questions.js
@@ -3,9 +3,32 @@ var router = express.Router();
 
 var QuestionsService = require('./questionsService');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+
+
+// GET ALL ANIMAL QUESTIONS
+router.route('/elaimet')
+  .get((req, res, next) => {
+    QuestionsService.getAllAnimalQuestions((results) => {
+      res.json(results)
+    })
+  })
+router.route('/historia')
+  .get((req, res, next) => {
+    QuestionsService.getAllHistoryQuestions((results) => {
+      res.json(results)
+    })
+  })
+router.route('/tiede')
+  .get((req, res, next) => {
+    QuestionsService.getAllScienceQuestions((results) => {
+      res.json(results)
+    })
+  })
+
 
 module.exports = router;
--- jeopardy-server/routes/questionsService.js
+++ jeopardy-server/routes/questionsService.js
@@ -9,4 +9,40 @@ const conopts = {
 
 const pool = new Pool(conopts);
 
-module.exports = {};
+function getAllAnimalQuestions(callback) {
+  pool.connect((err, client) => {
+    if (err) throw err;
+    client.query("SELECT category.title, question.title, question.difficulty FROM category JOIN question ON category.id = question.id AND category.title = 'Elaimet'",
+    (err, data) => {
+      if (err) throw err;
+      client.release();
+      callback(data.rows);
+    });
+  });
+}
+
+function getAllHistoryQuestions(callback) {
+  pool.connect((err, client) => {
+    if (err) throw err;
+    client.query("SELECT category.title, question.title, question.difficulty FROM category JOIN question ON category.id = question.id AND category.title = 'Historia'",
+    (err, data) => {
+      if (err) throw err;
+      client.release();
+      callback(data.rows);
+    });
+  });
+}
+
+function getAllScienceQuestions(callback) {
+  pool.connect((err, client) => {
+    if (err) throw err;
+    client.query("SELECT category.title, question.title, question.difficulty FROM category JOIN question ON category.id = question.id AND category.title = 'Tiede'",
+    (err, data) => {
+      if (err) throw err;
+      client.release();
+      callback(data.rows);
+    });
+  });
+}
+
+module.exports = { getAllAnimalQuestions, getAllHistoryQuestions, getAllScienceQuestions };
