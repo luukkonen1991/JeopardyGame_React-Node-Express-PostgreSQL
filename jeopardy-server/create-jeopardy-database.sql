023808ff66bed01310590de1af8d0fc15e9100cf
luukkonen1991 <luukkonen1991@gmail.com>
2019-11-19 09:54:12 +0200

    Database structure created

--- jeopardy-server/create-jeopardy-database.sql
+++ jeopardy-server/create-jeopardy-database.sql
@@ -6,20 +6,18 @@ CREATE DATABASE questions;
 CREATE TABLE question (
   id SERIAL PRIMARY KEY,
   title varchar(255) NOT NULL,
-  difficulty varchar(255) NOT NULL
-  -- FOREIGN KEY (id) REFERENCES category
+  difficulty varchar(255) NOT NULL,
+  category INT
 );
 
--- INSERT INTO question (title, difficulty) VALUES ("Otsikko", "Vaikea");
 
 CREATE TABLE options (
   id SERIAL PRIMARY KEY,
   title varchar(255) NOT NULL,
-  result boolean
-  -- FOREIGN KEY (id) REFERENCES question (id)
+  result boolean,
+  question INT
 );
 
--- INSERT INTO options (title, result) VALUES ("Eka vaihtoehto", false);
 
 CREATE TABLE category (
   id SERIAL PRIMARY KEY,
@@ -27,6 +25,24 @@ CREATE TABLE category (
   summary varchar(255) NOT NULL
 );
 
-\d category;
 
--- INSERT INTO category (title, summary) VALUES ("Eläimet", "category-kuvuas");
+
+INSERT INTO options (title, result) VALUES ('Eka vaihtoehto', false);
+INSERT INTO options (title, result) VALUES ('Toka vaihtoehto', true);
+INSERT INTO options (title, result) VALUES ('Kolmas vaihtoehto', false);
+
+INSERT INTO question (title, difficulty) VALUES ('Otsikko', 'Vaikea');
+
+INSERT INTO category (title, summary) VALUES ('Elaimet', 'category-kuvuas');
+
+
+
+
+ALTER TABLE question add CONSTRAINT fk_category FOREIGN KEY(category) REFERENCES category(id);
+
+ALTER TABLE options add CONSTRAINT fk_question FOREIGN KEY(question) REFERENCES question(id);
+
+
+
+
+\d options;
