9ca3c3a6b843dd66f5365888bccea1bf2dee933f
Saara Hiltunen <saara-lotta@hotmail.com>
2019-11-20 11:35:49 +0200

    saara

--- jeopardy-client/src/components/HistoryQuestions.js
+++ jeopardy-client/src/components/HistoryQuestions.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class HistoryQuestions extends Component {
+    state = {
+        historyQuestions: []
+
+    }
+
+    componentDidMount(){
+        fetch("api/questions/historia")
+        .then(res => res.json())
+        .then(res => this.setState({historyQuestions: res}))
+        .then(console.log(this.state.historyQuestions))
+        .catch(()=> this.setState({hasErrors: true}));
+    }
+
+    
     render() {
+        const historyQuestionItems = this.state.historyQuestions
+        .map((question) =>{
+            return <historyQuestionItems data = {question}/>
+        } )
         return (
             <div>
-                
+                 {historyQuestionItems}
             </div>
         );
     }
--- jeopardy-client/src/components/ScienceQuestions.js
+++ jeopardy-client/src/components/ScienceQuestions.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 
 class ScienceQuestions extends Component {
+    componentDidMount(){
+        fetch("api/questions/tiede")
+        .then(res => res.json())
+        .then(res => this.setState({scienceQuestions: res}))
+        .then(console.log(this.state.scienceQuestions))
+        .catch(()=> this.setState({hasErrors: true}));
+    }
     render() {
+        const scienceQuestionItems = this.state.scienceQuestions
+        .map((question) =>{
+            return <scienceQuestionItems data = {question}/>
+        } )
         return (
             <div>
                 
