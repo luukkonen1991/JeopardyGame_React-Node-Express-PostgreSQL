fdbc619d3f67a0b6c950a9c0ddcd8577142ccbe1
Ilkka Miettinen <ivmiettinen@gmail.com>
2019-11-20 10:02:28 +0200

    ItemitLisätty

---
+++ jeopardy-client/src/components/HistoryQuestionItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HistoryQuestionItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default HistoryQuestionItem;
---
+++ jeopardy-client/src/components/ScienceQuestionItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ScienceQuestionItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ScienceQuestionItem;
